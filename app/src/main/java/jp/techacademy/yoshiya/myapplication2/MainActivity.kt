3bb9c253be2ffd5248149bb6af85ec1fb5bd1ea7
inamori58 <dsmjoe5833@gmail.com>
2021-06-27 20:02:41 +0900

    Lesson3課題を再再提出します。

--- app/src/main/java/jp/techacademy/yoshiya/myapplication2/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yoshiya/myapplication2/MainActivity.kt
@@ -8,9 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val human = Human("山田", 25, hobby = "山登り")      // 名前を山田、年齢25歳、趣味は山登りで、Humanのインスタンスを作る
 
+        val human = Human("山田", 25, hobby = "山登り")  // 名前を山田、年齢25歳、趣味は山登りで、Humanのインスタンスを作る
         human.say()
-        human.think()
+
+        val human2 = Human("山田", 25, hobby = "山登り") // 名前を山田、年齢25歳、趣味は山登りで、Human2のインスタンスを作る
+        human2.think()
     }
 }
